b1c020b9f8f05d01e63ef3f041615c7ee7e20e9f
Marta <marta.nunes.abreu@gmail.com>
2020-11-21 16:48:42 +0000

    tidied up linked list implementation

--- data_structures/linked_list.c
+++ data_structures/linked_list.c
@@ -6,6 +6,24 @@ typedef struct node {
     struct node *next;
 } node;
 
+node *initialize_list()
+{
+    node *head;
+    head = (struct node*) malloc(sizeof head);
+    head->item = 0;
+    head->next = NULL;
+
+    return head;
+}
+
+void insert_node(node **head_ref, int x)
+{
+    node *new_node = (struct node*) malloc(sizeof new_node);         /* temporary pointer */
+    new_node->item = x;     /* initialise new node with an "item" */
+    new_node->next = *head_ref;    /* initialise new node with a "next" pointer */
+    *head_ref = new_node;          /* head now points to newly added item */
+}
+
 node *search_list_recursively(node *l, int x)
 {
     if (l == NULL)
@@ -29,110 +47,79 @@ node *search_list_iteratively(node *l, int x)
             printf("Error: predecessor sought on null list.\n");
             return(NULL);
         }
-        printf("item: %d", l->item);
         l = l->next;
     }
-    printf("found %d", l->item);
     return l;
 }
 
-void insert_node(node **l, int x)
+void delete_node(node **l, int x)
 {
-    node *temp;         /* temporary pointer */
-    temp = (struct node*) malloc(sizeof temp);
-    temp->item = x;     /* initialise new node with an "item" */
-    temp->next = *l;    /* initialise new node with a "next" pointer */
-    *l = temp;          /* head now points to newly added item */
-}
-
-node *get_previous_node(node *l, int x)
-{
-    if( l->item == x)
+    /* defensive programming - check if node exists in list 
+    node node_to_delete = search_list_iteratively(*l, x);
+    if (node_to_delete == NULL)
     {
-        return l;
+        printf("Error: Node not present in list");
+    } */
+
+    node *previous_node;
+    node *temp = *l;
+
+    // Check if node to delete is the head node
+    if( temp != NULL && temp->item == x)
+    {
+        *l = temp->next;
+        free(temp);
+        return;
     }
 
-    while( (l->next)->item != x)
+    while( temp != NULL && temp->item != x)
     {
-        l = l->next;
-        if( l->next == NULL)
+        previous_node = temp;
+        temp = temp->next;
+        if( temp->next == NULL)
         {
-            printf("Error: predecessor sought on null list.\n");
-            return(NULL);
+            printf("Error: Node not found in list.\n");
+            return;
         }
     }   
-    return l;
+    // Replace node to delete with the node next to it
+    previous_node->next = temp->next; 
+  
+    free(temp);  // Free memory 
 }
 
-void delete_list(node **l, int x)
+void print_list(node *node_ref)
 {
-    node *p; 
-    node *previous_node;
-    node *search_list_iteratively(), *get_previous_node();
-
-    p = search_list_iteratively(*l, x);
-
-    printf("Found node %d\n", p->item);
-    if (p != NULL)
-    {
-        previous_node = get_previous_node(*l, x);
-        if( previous_node == NULL)
-        {
-            /* node to delete is in HEAD */
-            *l = p->next;
-        }
-        else
-        {
-            printf("Deleting node %d \n", p->item);
-            previous_node->next = p->next; // doesn't get saved to list so it's wrong
-        }
-        free(p);
-    }
-    else
-    {
-        printf("Error: Couldn't find node \n");
-    }
-    
-
-}
-
-node *initialize_list()
-{
-    node *head;
-    head = (struct node*) malloc(sizeof head);
-    head->item = 0;
-    head->next = NULL;
-
-    return head;
+    printf("\nList ---------- \n");
+    while (node_ref != NULL) 
+    { 
+        printf(" %d \n", node_ref->item); 
+        node_ref = node_ref->next; 
+    } 
+    printf("\n");
 }
 
 int main(void)
 {
     node *list = initialize_list();
     
-    printf("List holds %d \n", list->item);
-
+    insert_node(&list, 1);
     insert_node(&list, 2);
-    printf("List holds %d \n", list->item);
-
-    insert_node(&list, 4);
-    printf("List holds %d \n", list->item);
-
     insert_node(&list, 3);
-    printf("List holds %d \n", list->item);
+    insert_node(&list, 4);
+    print_list(list);
 
-    search_list_iteratively(list, 2);
+   // node *to_find = search_list_iteratively(list, 2);
+   // printf("node next to 2: %d", to_find->next->item);
 
-    node *temp = get_previous_node(list, 4);
-    printf("Node previous to 4 is %d \n", temp->item);
+    //node *temp = get_previous_node(list, 2);
+    //printf("node previous to 2 is %d", temp->item);
 
-    //delete_list(&head, 4);
-    //search_list_iteratively(head, 2);    
-    delete_list(&list, 3);
-    delete_list(&list, 4);
-    delete_list(&list, 2);
-    //search_list_iteratively(list, 4);   
+    delete_node(&list, 2);
+    print_list(list);
 
-    //delete_list(&list, 3);
+    delete_node(&list, 4);
+    print_list(list);
+
 
 }
